5beab7070bf707bdf290258df95aba8cc587944a
ripee <ripee.dev@gmail.com>
2019-11-16 01:33:00 +0100

    zenltetmo: Set BTM_DEF_LOCAL_NAME

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,9 @@
 # Assert
 TARGET_OTA_ASSERT_DEVICE := zenltecan,zenltespr,zenltetmo
 
+# Bluetooth
+BOARD_BLUETOOTH_OID_BUILDCFG_INCLUDE_DIR := device/samsung/zenltetmo/bluetooth
+
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zenltetmo/include
 
