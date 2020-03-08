91e8b9678d34fc8f30cc4d14454244a03810fd2f
ripee <ripee.dev@gmail.com>
2020-03-08 21:31:07 +0100

    zenltetmo: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_zenltetmo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zenltetmo-user \
+    lineage_zenltetmo-userdebug \
+    lineage_zenltetmo-eng
