ebc2469f519a48e953c511738ad54cfce62a42b5
ripee <ripee.dev@gmail.com>
2020-07-16 14:55:07 +0200

    zenltetmo: Define BT name in common tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,9 +29,6 @@
 # Assert
 TARGET_OTA_ASSERT_DEVICE := zenltecan,zenltetmo
 
-# Bluetooth
-BOARD_BLUETOOTH_OID_BUILDCFG_INCLUDE_DIR := device/samsung/zenltetmo/bluetooth
-
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zenltetmo/include
 
